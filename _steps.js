bda59164c650427a1c8057641908c9bd59ead7a0
RHN007 <rakibul.nayon@outlook.com>
2022-10-30 09:38:09 +0600

    adding

---
+++ _steps.js
@@ -0,0 +1,9 @@
+/**
+ * POST
+ 1. Create POST API 
+ 2. Use fetch with method POST
+ 3. Add header to the fetch : 'content-type' : 'application/json'
+ 4. add body to the fetch to send data . 
+ 5. Make sure to ue JSON.Stringify to send data 
+ 6. use express.json as middleware (IMPORTANT)
+ */
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express'); 
+const app = express(); 
+const cors = require('cors'); 
+const port = process.env.PORT || 5002; 
+
+app.get('/', (req, res)=> {
+    res.send('Simple Node server is running ')
+})
+
+app.use(cors())
+app.use(express.json())
+const users = [
+    {id: 1, name: 'Sabana', email: 'Sabana@gmail.com'}, 
+    {id: 2, name: 'Bobita', email: 'bobita@gmail.com'}, 
+    {id: 3, name: 'Sabnur', email: 'sabnur@gmail.com'}, 
+]
+
+app.get('/users', (req, res)=> {
+    if(req.query.name){
+            const search = req.query.name; 
+            const filtered = users.filter(usr => usr.name.toLowerCase.indexOf(search)>=0)
+            res.send(filtered)
+    }
+    else{
+        res.send(users)
+    }
+})
+app.post('/users', (req, res) => {
+    const user = req.body; 
+    user.id == users.length + 1; 
+    users.push(user); 
+    console.log(user)
+    res.send(user)
+})
+
+app.listen(port, ()=> {
+    console.log(`Simple not server running on Port ${port}`)
+})
